896c5a6d38b391c972ee25d6b8edf8f38944a2b2
Abakar <abakar61@web.de>
2024-10-12 01:54:00 +0200

    Added some endpoints

--- src/main/java/com/bytmasoft/dss/controller/BFFControllerApi.java
+++ src/main/java/com/bytmasoft/dss/controller/BFFControllerApi.java
@@ -1,12 +1,15 @@
 package com.bytmasoft.dss.controller;
 
+import com.bytmasoft.dss.dto.StudentDetailCreateDTO;
 import com.bytmasoft.dss.dto.StudentDetailDTO;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 
 
 public interface BFFControllerApi {
@@ -14,5 +17,16 @@ public interface BFFControllerApi {
     @GetMapping("student-details/{studentId}")
     public ResponseEntity<StudentDetailDTO> getStudentDetails(@PathVariable Long studentId,
                                                             @RequestHeader (HttpHeaders.AUTHORIZATION) String authorizationHeader);
+
+
+    @PostMapping(value = "/pictures", consumes = "multipart/form-data")
+    ResponseEntity<StudentDetailDTO> addStudentWithPicture(
+            @RequestPart(value = "studentCreateDto") @Parameter(schema =@Schema(type = "string", format = "binary")) final StudentDetailCreateDTO studentCreateDto,
+            @Parameter(description = "Student picture")  @RequestPart(value = "picture") MultipartFile picture) throws IOException;
+
+    @PostMapping(value = "/pictures/add", consumes = "multipart/form-data")
+    ResponseEntity<StudentDetailDTO> addPictureToStudent(@Parameter(description = "Student ID") @RequestParam("studentId") Long studentId,
+                                                   @Parameter(description = "Student picture")   @RequestPart("file") MultipartFile file) throws IOException;
+
 
 }
